78c3ea7ce191988d7946306da2b887ae75660c60
ant_reyn <antreyn@udel.edu>
2022-03-23 16:00:43 -0400

    setting up the layout for the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>Let's get started!</h2>
+    </div>
+  );
 };
 
 export default App;
