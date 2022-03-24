2c9cdc349a2d18ed4764c2c314922d8a1217c2b0
ant_reyn <antreyn@udel.edu>
2022-03-23 20:58:53 -0400

    added logic for individual meal items and displaying them

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,4 +1,7 @@
+import React from "react";
 import "./AvailableMeals.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
   {
@@ -29,12 +32,14 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => {
-    return <li id={meal.id}>{meal.name}</li>;
+    return <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} />;
   });
 
   return (
     <section className="meals">
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
